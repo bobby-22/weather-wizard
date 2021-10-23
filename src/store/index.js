2e94045403628b40dc83f11a420d5751b86e15b4
bobby-22 <nlongn22@icloud.com>
2021-10-23 22:02:06 +0200

    Add default city

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        latitude: null,
-        longitude: null,
-        locationName: null,
-        locationImageURL: null,
+        latitude: 40.7128,
+        longitude: -74.0059728,
+        locationName: "New York",
+        locationImageURL:
+            "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sAap_uEBfp6BWVd06gQuQskn8eeW3v5OK009bY6j4HI3x-cxJ2an9T2qMarBCoq6C94x9l_qR095LWhJUyL9SMj2IZ-rUXAjdrqiFTr87Ja9O_FcW7iEO3DbX1ubFhh3xwiQptH7Int_8wwYBwHlnaFFWDvEytLxKXg1NDaZU7pIlseppuscM&3u3648&5m1&2e1&callback=none&key=AIzaSyDZtsUUOMZsh34qXB5NOXRPS0EXI7bNCZI&token=112369",
     },
     mutations: {
         initLatitude(state) {
