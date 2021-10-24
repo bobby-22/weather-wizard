621604db913996249bb0af62e04148ccdcece4ef
bobby-22 <nlongn22@icloud.com>
2021-10-24 14:05:26 +0200

    Fix Google Maps autocomplete not showing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount("#app");
+
+export const loadedGoogleMapsAPI = new Promise((resolve, reject) => {
+    window["GoogleMapsInit"] = resolve;
+
+    let GMap = document.createElement("script");
+
+    GMap.setAttribute(
+        "src",
+        `https://maps.googleapis.com/maps/api/js?key=${process.env.VUE_APP_GOOGLE_MAPS_API_KEY}&libraries=places`
+    );
+
+    document.body.appendChild(GMap);
+});
