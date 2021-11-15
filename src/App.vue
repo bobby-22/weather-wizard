8983a3f1bae2af16ae5e01549fb90957eba63285
bobby-22 <nlongn22@icloud.com>
2021-11-15 17:00:07 +0100

    Revert previous changes

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap");
 * {
     font-family: "Comfortaa", cursive;
 }
