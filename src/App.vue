e382583b2c2bc5e15bef4ec45e14e6437ef13a66
bobby-22 <nlongn22@icloud.com>
2021-11-15 16:38:51 +0100

    Import Google font using link

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,8 @@ export default {
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap");
 * {
-    font-family: "Comfortaa", sans-serif;
+    font-family: "Comfortaa", cursive;
 }
 .container {
     width: 100%;
